69bfdcd17e1a1b71315387de33a65e16d65595d4
S <s>
2012-01-16 01:06:56 +0530

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -247,7 +247,7 @@ public class Breakout extends GraphicsProgram {
 	private void lifeLost(){
 		if(lives_remaining>0){
 			remove(ball);
-			GLabel label=new GLabel("lives remaning "+lives_remaining);
+			GLabel label=new GLabel("lives remaning "+(lives_remaining-1));
 			add(label,(WIDTH-label.getWidth())/2,(HEIGHT-label.getAscent())/2);
 			pause(1000);
 			vy=-vy;
